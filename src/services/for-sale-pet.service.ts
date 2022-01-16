19ca57e8b3862c82811064eb464dd426f4f97ea8
Jay Philip <jayphilipcamillo@gmail.com>
2022-01-16 14:14:12 +0800

    Sort the for sale pets from latest

--- src/services/for-sale-pet.service.ts
+++ src/services/for-sale-pet.service.ts
@@ -42,7 +42,8 @@ const getAll = async (urlQuery: any, perPage: number = 1) => {
     { $match: query },
     { $limit: perPage + 1 },
     { $lookup: { from: "kennels", localField: "kennel", foreignField: "_id", as: "kennel" } },
-    { $unwind: "$kennel" }
+    { $unwind: "$kennel" },
+    { $sort: { createdAt: -1 } }
   ]);
   const results = forSalePets.length > perPage ? forSalePets.slice(0, -1) : forSalePets;
   const cursor = forSalePets.length > perPage ? forSalePets[forSalePets.length - 2]?._id : forSalePets[forSalePets.length - 1]?._id;
